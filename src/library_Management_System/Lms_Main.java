4cee2ea239ea352765da1d295f39e3e6509767fa
Sujan Rijal <rijalsujan@gmail.com>
2022-12-12 22:21:49 -0500

    Second Commit

---
+++ src/library_Management_System/Lms_Main.java
@@ -0,0 +1,10 @@
+package library_Management_System;
+
+public class Lms_Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author rijalsujan09
+ *
+ */
+module Java_level2_Project {
+}
