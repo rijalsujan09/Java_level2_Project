14f87971c9e0919da00c4fe371865b4a307ca33b
Sujan Rijal <rijalsujan@gmail.com>
2022-12-12 23:04:17 -0500

    My last Commit

--- src/library_Management_System/Lms_Main.java
+++ src/library_Management_System/Lms_Main.java
@@ -1,9 +1,21 @@
 package library_Management_System;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class Lms_Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		String Book[] = { "Maths", "physic", "Chemestry", "Biology", "Nepali", "computer" };
+
+		Deque<String> myBooks = new LinkedList<>();
+
+		for (int i = 0; i < Book.length; i++) {
+			myBooks.offer(Book[i]);
+		}
+
+		System.out.println(myBooks);
 
 	}
 
